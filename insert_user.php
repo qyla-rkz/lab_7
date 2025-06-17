30ea43ea4196bfab374651bc857b349c644d539f
Nurul Aqilah Khairuni <aqilahkz2003@gmail.com>
2025-06-17 19:56:27 +0800

    update insert_user.php

--- insert_user.php
+++ insert_user.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "lab7");
+$conn = new mysqli("localhost", "root", "", "lab_7");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
